5f415ca890b1298c536840dc713ae53c3e9812f4
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-05-11 04:33:34 +0300

    Update maker-quote.js

--- plugins/maker-quote.js
+++ plugins/maker-quote.js
@@ -97,11 +97,8 @@ let handler = async (m, { conn, text }) => {
     }
 
     fs.unlinkSync(tempImagePath)
-
-    m.react('🤡')
   } catch (e) {
-    console.error(e)
-    m.react('😭')
+    console.error(e) 
   }
 }
 
